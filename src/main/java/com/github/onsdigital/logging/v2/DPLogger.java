dbcb6c20a52593981ca712bed45b5e7b7c9df2c3
Dave Llewellyn <439837@gmail.com>
2019-08-07 17:23:55 +0100

    added more unit tests to improve coverage and new features

--- src/main/java/com/github/onsdigital/logging/v2/DPLogger.java
+++ src/main/java/com/github/onsdigital/logging/v2/DPLogger.java
@@ -3,6 +3,7 @@ package com.github.onsdigital.logging.v2;
 import com.github.onsdigital.logging.v2.config.DefaultConfig;
 import com.github.onsdigital.logging.v2.config.ErrorWriter;
 import com.github.onsdigital.logging.v2.config.LogConfig;
+import com.github.onsdigital.logging.v2.config.ShutdownHook;
 import com.github.onsdigital.logging.v2.event.BaseEvent;
 import com.github.onsdigital.logging.v2.event.Severity;
 
@@ -48,31 +49,33 @@ public class DPLogger {
     }
 
     public static <T extends BaseEvent> void log(T event) {
-        String eventJson = null;
         try {
-            eventJson = marshalEvent(event);
-        } catch (LoggingException ex) {
-            handleMarshalEventFailure(event, ex);
-            return;
-        }
+            String eventJson = marshalEvent(event);
+            Severity severity = getSeverity(event.getSeverity());
 
-        Severity severity = getSeverity(event.getSeverity());
-        logEvent(eventJson, severity, logConfig().getLogger());
+            logEventJson(eventJson, severity, logConfig().getLogger());
+        } catch (LoggingException ex) {
+            ErrorWriter errorWriter = logConfig().getErrorWriter();
+            ShutdownHook shutdownHook = logConfig().getShutdownHook();
+
+            handleMarshalEventFailure(event, ex, errorWriter, shutdownHook);
+        }
     }
 
     private static <T extends BaseEvent> String marshalEvent(T event) throws LoggingException {
         return logConfig().getSerialiser().marshallEvent(event);
     }
 
-    private static <T extends BaseEvent> void handleMarshalEventFailure(T event, LoggingException ex) {
-        ErrorWriter errorWriter = logConfig().getErrorWriter();
-        boolean isError = errorWriter.write(format(MARSHAL_FAILURE, event, ex));
+    static <T extends BaseEvent> void handleMarshalEventFailure(T event, LoggingException ex,
+                                                                ErrorWriter errorWriter,
+                                                                ShutdownHook shutdownHook) {
+        boolean isError = errorWriter.write(getMarshalFailureMessage(event, ex));
         if (isError) {
             logConfig().getShutdownHook().shutdown();
         }
     }
 
-    private static void logEvent(String eventJson, Severity severity, Logger logger) {
+    static void logEventJson(String eventJson, Severity severity, Logger logger) {
         if (FATAL == severity)
             logger.error(eventJson);
         else if (ERROR == severity)
@@ -81,6 +84,10 @@ public class DPLogger {
             logger.warn(eventJson);
         else
             logger.info(eventJson);
+    }
+
+    static <T extends BaseEvent> String getMarshalFailureMessage(T event, LoggingException ex) {
+        return format(MARSHAL_FAILURE, event, ex);
     }
 
     public static DateTimeFormatter formatter() {
